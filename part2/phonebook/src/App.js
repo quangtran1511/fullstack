49457654db50f3dc30e082927c3749ab12e97428
Tran Anh (e1900305) <namanh022@gmail.com>
2022-06-19 20:10:34 +0300

    completed 2.11

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,17 +1,21 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import Filter from './components/Filter'
 import PersonForm from './components/PersonForm'
 import Persons from './components/Persons'
+import axios from 'axios'
+
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ]) 
+  const [notes, setNotes] = useState([])
+  const [persons, setPersons] = useState() 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('');
   const [search, setSearch] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:3001/persons")
+      .then(response => { setPersons(response.data) })
+  }, [])
 
   const handleInputName = (e) => {
     e.preventDefault();
@@ -61,7 +65,7 @@ const App = () => {
         persons?.map((person) =>
         person.name.toLowerCase().includes(search.toString().toLowerCase()) ?
         (
-          <Persons name={person.name} number={person.number } />
+              <Persons name={person.name} number={person.number} key={ person.id} />
         ) : ("")
         )
       }  
