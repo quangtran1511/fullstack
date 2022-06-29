3769f743b9d260da59d1b459353a60e33df2bd0a
Tran Anh (e1900305) <namanh022@gmail.com>
2022-06-29 17:22:57 +0300

    finished 3.18

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,86 +1,122 @@
 const { response } = require("express");
 const express = require("express");
 const morgan = require("morgan") 
-
+const Person =require('./models/person')
 const app = express();
 let cors = require("cors");
+
+
 morgan.token('body', (req, ) => JSON.stringify(req.body))
 app.use(morgan(":method :url :status :res[content-length] - :response-time ms :body"))
 app.use(cors());
 app.use(express.json());
+app.use(express.static('build'))
 
-let data= [
-    { 
-        "id": 1,
-        "name": "Arto Hellas", 
-        "number": "040-123456"
-      },
-      { 
-        "id": 2,
-        "name": "Ada Lovelace", 
-        "number": "39-44-5323523"
-      },
-      { 
-        "id": 3,
-        "name": "Dan Abramov", 
-        "number": "12-43-234345"
-      },
-      { 
-        "id": 4,
-        "name": "Mary Poppendieck", 
-        "number": "39-23-6423122"
-      }
-]
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === "ValidationError") {
+    return response.status(400).json({ error: error.message });
+    next(error)
+  }
+}
 
 app.get('/api/persons', (request, response) => {
-    response.json(data);
+    Person.find({}).then(notes => {
+    response.json(notes)
+  })
+
 })
+
 app.get("/info", (request, response) => {
-    response.send(`<div>Phonebook has info for ${data.length} people</div>
-        <div>${new Date()}</div>`)
-})
-app.get("/api/persons/:id", (request, respone) => {
-    const id = Number(request.params.id)
-    const person = data.find(person => person.id === id)
-    if (person) {
-      respone.json(person)
-    } else {
-      respone.status(404).end()
-  }
+  Person.find({}).then((persons) => {
+    response.send(`<div>Phonebook contains ${persons.length} people</div>
+    <div>${new Date()}</div>`)
+  })
 })
 
-app.delete("/api/persons/:id", (request, respone) => {
-  const id = Number(request.params.id)
-  person  = data.filter(person => person.id !== id)
-
-  respone.status(204).end()
+app.get('/api/persons/:id', (request, response, next) => {
+  Person.findById(request.params.id)
+    .then((person) => {
+      if (person) {
+        response.json(person)
+      } else {
+        response.status(404).end()
+      }
+    })
+    .catch((error) => next(error))
 })
 
-const generateId = () => {
-  const maxId = Math.floor(Math.random() * 100);
-  return maxId
-}
-app.post("/api/persons", (request, response) => {
+app.delete("/api/persons/:id", (request, respone,next) => {
+  Person.findByIdAndRemove(request.params.id)
+    .then(result => {
+      respone.status(204).end()
+    }).catch(error=>next(error))
+})
+
+//previous excercise
+// const generateId = () => {
+//   const maxId = Math.floor(Math.random() * 10000);
+//   return maxId
+
+// app.post("/api/persons", (request, response) => {
+//   const body = request.body
+//   const isExist = data.find(person=>person.name===body.name)
+//   if (!body.name && !body.number) {
+//     return response.status(400).json({
+//       error:"missing name or number"
+//     })
+//   }
+//   if (isExist) {
+//     return response.status(400).json({
+//       error:"name must be unique.Please change another name"
+//     })
+//   }
+//   const person = {
+//     name: body.name,
+//     number: body.number,
+//     id: generateId()
+//   }
+//   data = data.concat(person)
+//   response.json(person)
+// })
+
+  //ex 3.14
+  app.post("/api/persons", (request, response) => {
+    const body = request.body
+    const newPerson = new Person({
+      name: body.name,
+      number:body.number
+    })
+    newPerson.save().then(savedPerson => {
+      response.json(savedPerson)
+    })
+  })
+
+app.put("/api/persons/:id", (request, response,next) => {
+  const id = request.params.id
   const body = request.body
-  const isExist = data.find(person=>person.name===body.name)
-  if (!body.name && !body.number) {
-    return response.status(400).json({
-      error:"missing name or number"
-    })
-  }
-  if (isExist) {
-    return response.status(400).json({
-      error:"name must be unique.Please change another name"
-    })
-  }
   const person = {
     name: body.name,
-    number: body.number,
-    id: generateId()
+    number:body.number
   }
-  data = data.concat(person)
-  response.json(person)
-})
+  Person.findByIdAndUpdate(id, person, { new: true })
+    .then(updatedPerson => {
+      response.json(updatedPerson)
+    })
+    .catch(error => next(error))
+  })
+
+  const unknownEndpoint = (request, response) => {
+    response.status(404).send({ error: 'unknown endpoint' })
+  }
+  
+app.use(unknownEndpoint)
+
+// this has to be the last loaded middleware.
+app.use(errorHandler)
 
 const PORT =process.env.PORT || 3001
 app.listen(PORT, () => {
