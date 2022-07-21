b2e5c21b2695bd7a7e44ec5a562d62aeece3cb12
Tran Anh (e1900305) <namanh022@gmail.com>
2022-07-21 16:18:46 +0300

    updated 4.20

--- part4/blogList-backend/controllers/blogs.js
+++ part4/blogList-backend/controllers/blogs.js
@@ -1,15 +1,6 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blogs')
-const User = require('../models/users')
-const jwt = require('jsonwebtoken')
 
-const getTokenFrom = request => {
-  const authorization = request.get('authorization')
-  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
-    return authorization.substring(7)
-  }
-  return null
-}
 
 blogsRouter.get('/', async(request, response) => {
     const blogs = await Blog.find({}).populate('user')
@@ -18,13 +9,13 @@ blogsRouter.get('/', async(request, response) => {
   
 blogsRouter.post('/', async(request, response,next) => {
   const body = request.body
-  
-  const token = getTokenFrom(request)
-  const decodedToken = jwt.verify(token, process.env.SECRET)
-  if (!decodedToken.id) {
-    return response.status(401).json({ error: 'token missing or invalid' })
+  const token = request.token
+  const user = request.user
+  console.log("user", user)
+  console.log("token", token)
+  if (!(token && user)) {
+    return response.status(401).json({ error: 'token missing or invalid' }) 
   }
-  const user = await User.findById(decodedToken.id)
 
   const blog = new Blog({
     title: body.title,
--- part4/blogList-backend/utils/middleware.js
+++ part4/blogList-backend/utils/middleware.js
@@ -1,4 +1,6 @@
 const logger = require('./logger')
+const User = require('../models/users')
+const jwt = require('jsonwebtoken')
 
 const requestLogger = (request, response, next) => {
   logger.info('Method:', request.method)
@@ -24,8 +26,33 @@ const errorHandler = (error, request, response, next) => {
   next(error)
 }
 
+const getTokenFrom = request => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+    return authorization.substring(7)
+  }
+  return null
+}
+
+const tokenExtractor = (request, response, next) => {
+  request.token = getTokenFrom(request)
+  next()
+}
+
+const userExtractor = async (request, response, next) => {
+  const token = getTokenFrom(request)
+  console.log("token middle", token)
+  const decodedToken = token ? jwt.verify(token, process.env.SECRET) : null
+  
+  request.user = decodedToken ? await User.findById(decodedToken.id) : null
+  
+  next()
+}
+
 module.exports = {
   requestLogger,
   unknownEndpoint,
-  errorHandler
+  errorHandler,
+  tokenExtractor,
+  userExtractor
 }
